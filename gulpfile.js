bdd55979ff2890168c93fcf25bac10c6d57fb0ff
Gerardo Bort <gerardobort@gmail.com>
2017-08-27 00:33:54 -0300

    build(gulpfile): prepublish fixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,17 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const copy = require('gulp-copy');
 
-gulp.task('default', function () {
+gulp.task('default', ['copy', 'transpile']);
+
+gulp.task('transpile', function () {
   return gulp.src(['src/*', 'src/**/*.js'])
     .pipe(babel())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('copy', function () {
+  return gulp.src(['src/**/*.json'])
+    .pipe(copy('./dist', { prefix: 1 }))
     .pipe(gulp.dest('dist'));
 });
