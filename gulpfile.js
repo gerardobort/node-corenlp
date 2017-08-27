0901790378324271dd58a5e3f56a36b1f0c67e5d
Gerardo Bort <gerardobort@gmail.com>
2017-08-27 00:18:04 -0300

    build(gulpfile): fixing compile task

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 
 gulp.task('default', function () {
-  return gulp.src('src/index.js')
+  return gulp.src(['src/*', 'src/**/*.js'])
     .pipe(babel())
     .pipe(gulp.dest('dist'));
 });
