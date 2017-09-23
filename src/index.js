e6bbd7f50c438385d0203caa6c699cc9a5731e03
Gerardo Bort <gerardobort@gmail.com>
2017-09-23 13:30:47 -0300

    docs(README.md): updating documentation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import Properties from './properties';
-import Pipeline from './pipeline';
 import Document from './simple/document';
 import Sentence from './simple/sentence';
 import Token from './simple/token';
@@ -14,24 +12,23 @@ import ParserAnnotator from './simple/annotator/parse';
 import DependencyParseAnnotator from './simple/annotator/depparse';
 import RelationExtractorAnnotator from './simple/annotator/relation';
 import RegexNERAnnotator from './simple/annotator/regexner';
-import Service from './service';
-import ConnectorServer from './connector/connector-server';
-import ConnectorCli from './connector/connector-cli';
 import Tree from './util/tree';
+import _Properties from './properties';
+import _Pipeline from './pipeline';
+import _Service from './service';
+import _ConnectorCli from './connector/connector-cli';
+import _ConnectorServer from './connector/connector-server';
+
+export const Properties = _Properties;
+export const Pipeline = _Pipeline;
+export const Service = _Service;
+export const ConnectorCli = _ConnectorCli;
+export const ConnectorServer = _ConnectorServer;
 
 /**
  * CoreNLP NodeJS Interface
  */
 export default {
-  connector: {
-    ConnectorServer,
-    ConnectorCli,
-  },
-
-  Properties,
-  Pipeline,
-  Service,
-
   /**
    * https://stanfordnlp.github.io/CoreNLP/simple.html
    */
